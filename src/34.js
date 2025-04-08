adeb563c830b41d6a257cc8005d5e71fcf0216cd
columbowalstad130 <columbowalstad130@gmail.com>
2025-04-08 12:27:39 +0000

    Add 34.js

---
+++ src/34.js
@@ -0,0 +1,6 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+// Example usage: Generate a random integer between 1 and 10 (inclusive)
+console.log(getRandomInt(10));
